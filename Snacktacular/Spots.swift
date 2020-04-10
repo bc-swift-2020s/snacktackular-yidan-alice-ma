8015b92c7e126687d3f22e823422c0e5ae5028a9
Yidan Ma <madt@bc.edu>
2020-04-10 18:23:38 -0400

    9.2 - UI tour + Creating the Spot and Spots classes

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by mayidan on 4/10/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
